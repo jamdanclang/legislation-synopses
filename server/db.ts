47e20ae3b16abdb165877c51d4a809e3ed027c1d
Jamdan Clang <jamdanclang@gmail.com>
2025-08-09 16:20:01 -0500

    Delete server directory

--- server/db.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import pkg from 'pg';
-const { Pool } = pkg;
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export async function query(q: string, params?: any[]) {
-  const res = await pool.query(q, params);
-  return res.rows;
-}
